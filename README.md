aa3ed093f6997ec7beb0d65b2c761d4852a79c14
ibrahim Dagci <iburamincoldd@gmail.com>
2023-02-17 01:51:57 +0300

    First Commit: Java Script Demo

---
+++ README.md
@@ -0,0 +1,5 @@
+# First JS Demo
+
+- EN: This is a java script test for Çay Core
+
+- TR: JS denemesidir. (ödev 4) Çay Core 4. hafta.
---
+++ main.js
@@ -0,0 +1,36 @@
+// js demo page.
+// variables
+var randomNumber = 5;
+var randomNumber2 = randomNumber;
+var a; //undefined
+// NaN -> Not a Number
+
+// numbers
+var number1 = 7;
+var number2 = 4;
+
+// functions
+
+function attention() {
+    alert("Hello world!:)");
+}
+
+function sum(){
+    alert(number1 + number2);
+}
+
+function diff(){
+    alert(number1 - number2);
+}
+
+function multipl(){
+    alert(number1 * number2);
+}
+
+function division(){
+    alert(number1 / number2)
+}
+
+function remainder(){
+    alert(number1 % number2);
+}
